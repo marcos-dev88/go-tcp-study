5b4e860870118e0780578256bd2b3d24d24407ff
Marcos Vitor <mv.piesske@gmail.com>
2021-12-12 21:57:32 -0300

    Updating data format body to use channels and go routines

--- tcp_server/server.go
+++ tcp_server/server.go
@@ -8,6 +8,7 @@ import (
 	"net"
 	"regexp"
 	"strings"
+	"sync"
 )
 
 type Message struct {
@@ -17,11 +18,46 @@ type Message struct {
 
 func Handle(conn net.Conn) {
 
+	byteDataCH, formattedBodyCH := make(chan []byte, 3072), make(chan string, 2)
+
+	wg := sync.WaitGroup{}
+	wg.Add(2)
+
+	defer wg.Wait()
 	defer conn.Close()
 
-	var dataBytes = make([]byte, 3072)
+	go getByteDataFromConn(conn, byteDataCH, &wg)
+	go formatData(byteDataCH, formattedBodyCH, &wg)
+
+	body := getDataFromBody(<-formattedBodyCH)
+
+	if len(body) != 0 {
+		fmt.Printf("%v | %T", body["value"], body["value"])
+	}
+
+}
+
+func formatData(inputData <-chan []byte, outputData chan string, newWg *sync.WaitGroup) {
+	defer newWg.Done()
+
+	var rgex = regexp.MustCompile(`Length: \d+(.*)`)
+
+	stringLine := string(<-inputData)
+	dataBody := rgex.FindStringSubmatch(stringLine)
+	if dataBody != nil {
+		returnData := strings.Replace(dataBody[1], "{", "", -1)
+		returnData = strings.Replace(returnData, "}", "", -1)
+		outputData <- returnData
+	}
+	close(outputData)
+}
+
+func getByteDataFromConn(conn net.Conn, inputData chan []byte, newWg *sync.WaitGroup) {
+	defer newWg.Done()
+
 	sc := bufio.NewScanner(conn)
 
+	var dataBytes = make([]byte, 3072)
 	for sc.Scan() {
 		dataBytes = append(dataBytes, sc.Bytes()...)
 		if bytes.Contains(dataBytes, []byte("}")) {
@@ -29,41 +65,22 @@ func Handle(conn net.Conn) {
 		}
 	}
 
-	formatedLine := formatLine(dataBytes)
-	body := getDataFromLine(formatedLine)
-
-	if len(body) != 0 {
-		fmt.Printf("%v | %T", body["value"], body["value"])
-	}
-
 	if err := sc.Err(); err != nil {
 		log.Fatal(err)
 	}
 
+	inputData <- dataBytes
+	close(inputData)
 }
 
-func formatLine(line []byte) string {
-	var rgex = regexp.MustCompile(`Length: \d+(.*)`)
-
-	stringLine := string(line)
-	data := rgex.FindStringSubmatch(stringLine)
-	if data != nil {
-		returnData := strings.Replace(data[1], "{", "", -1)
-		returnData = strings.Replace(returnData, "}", "", -1)
-		return returnData
-	}
-	return ""
-}
-
-func getDataFromLine(line string) map[string]interface{} {
-	s := strings.Split(line, ",")
+func getDataFromBody(body string) map[string]interface{} {
+	dataSlice := strings.Split(body, ",")
 
 	var bodyMap = make(map[string]interface{})
-
 	var rgex = regexp.MustCompile(`("\w+"): (.*)`)
 
-	for i := 0; i < len(s); i++ {
-		data := rgex.FindStringSubmatch(s[i])
+	for i := 0; i < len(dataSlice); i++ {
+		data := rgex.FindStringSubmatch(dataSlice[i])
 		if data != nil {
 			key := strings.Replace(data[1], `"`, "", -1)
 			bodyMap[key] = data[2]
